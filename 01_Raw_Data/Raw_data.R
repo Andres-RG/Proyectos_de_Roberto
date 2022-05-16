aa311090d9fb264546e502f84c060149311bac8a
Andres-RG <lrojas26@alumnos.uaq.mx>
2022-05-16 15:02:41 -0500

    Se actualizó la base de datos al día 8 de abril del 2022. Se obtuvo el número de casos positivos por fecha de inicio de síntomas por día de la nueva base de datos.

--- 01_Raw_Data/Raw_data.R
+++ 01_Raw_Data/Raw_data.R
@@ -1,1 +1,2 @@
 load("01_Raw_Data/datos_covid_qro.RData")
+load("01_Raw_Data/datos_covidmx.RData")
--- 02_Scripts/Script.R
+++ 02_Scripts/Script.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(viridisLite)
 library(viridis)
 library(deSolve)
+library(ape)
 source("01_Raw_Data/Raw_data.R")
 source("03_Functions/Functions.R")
 
@@ -88,3 +89,63 @@ muerte <- filter(positivos_m, muerte == "Muerte")
 
 p_m<- probabilidades(muerte, positivos_re)
 p_m
+
+
+## TABLA CONJUNTA ===========================================
+p_t <- cbind(p_p, p_l, p_h, p_i, p_m)
+colnames(p_t) <- c("Suceptible --> Infectado",
+                      "Infectado --> Ambulatorio",
+                      "Infectado --> Grave",
+                      "Grave --> ICU",
+                      "ICU --> Muerte")
+
+## Correlación de las probabilidades =======================
+
+heatmap_p_t <- heatmap(cor(t(p_t)))
+
+## Analisis de clasificación ===============================
+d_p_t <- dist(p_t)
+clu <- hclust(d_p_t, method = "complete", members = NULL)
+clu_p <- plot ( as.phylo  (clu), type = "phylogram" )
+
+### Robustez de la clasificacion
+
+agroup1 <- hclust(d_p_t, method = "complete", members = NULL)
+agroup2 <- hclust(d_p_t, method = "ward.D", members = NULL)
+agroup3 <- hclust(d_p_t, method = "ward.D2", members = NULL)
+agroup4 <- hclust(d_p_t, method = "single", members = NULL)
+agroup5 <- hclust(d_p_t, method = "average", members = NULL)
+agroup6 <- hclust(d_p_t, method = "mcquitty", members = NULL)
+agroup7 <- hclust(d_p_t, method = "median", members = NULL)
+agroup8 <- hclust(d_p_t, method = "median", members = NULL)
+agroup9 <- hclust(d_p_t, method = "centroid", members = NULL)
+#   3) Graficar usando la funcion: plot ()
+#      Para visualizar llos analisis usando layout
+layout ( matrix ( c( 1 : 9 ), 3, 3))
+plot(agroup1)
+plot(agroup2) 
+plot(agroup3)
+plot(agroup4)
+plot(agroup5) 
+plot(agroup6) 
+plot(agroup7)
+plot(agroup8)
+plot(agroup9)
+layout(matrix (c (1), 1, 1))
+
+## ACTUALIZACION DE DATOS AL 8-04-2022=======================================++
+datos_covidmx
+datos_covid_qro_act_8_04_2022 <- filter(datos_covid_qro, ENTIDAD_UM == 22)
+positivos_act <- filter(datos_covid_qro_act_8_04_2022, CLASIFICACION_FINAL == 1 | 
+                      CLASIFICACION_FINAL == 2 |
+                      CLASIFICACION_FINAL == 3 )
+pos <- c()
+for (i in 1:length(positivos_act$FECHA_SINTOMAS) ) {
+  pos <- c(pos,1)
+}
+positivos_act <- mutate(positivos_act, positivos = pos)
+p <- aggregate(positivos~FECHA_SINTOMAS, data = positivos_act,
+               FUN = sum)
+p[,3] <- c(1:length(p$FECHA_SINTOMAS))
+colnames(p)[3] <- "num.dia"
+print(p)
